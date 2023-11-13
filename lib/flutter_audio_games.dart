5e7c2d2cb479847b93a2f266330182fb7d911eb2
Chris Norman <chris.norman2@googlemail.com>
2023-11-13 18:42:44 +0000

    Renamed a directory.

--- lib/flutter_audio_games.dart
+++ lib/flutter_audio_games.dart
@@ -7,6 +7,6 @@ export 'widgets/menus/audio_game_menu_item_list_tile.dart';
 export 'widgets/music_builder/inherited_music_builder.dart';
 export 'widgets/music_builder/music_builder.dart';
 export 'widgets/play_sound_semantics.dart';
-export 'widgets/ticking/inherited_ticking_builder.dart';
-export 'widgets/ticking/ticking_builder.dart';
+export 'widgets/ticking_builder/inherited_ticking_builder.dart';
+export 'widgets/ticking_builder/ticking_builder.dart';
 export 'widgets/timed_builders.dart';
