da321876667b9e277124221c1fbbb419d9fec05c
Chris Norman <chris.norman1989@outlook.com>
2024-05-22 11:31:17 +0100

    Fixed filename of the `main_level` asset.

--- example/lib/gen/assets.gen.dart
+++ example/lib/gen/assets.gen.dart
@@ -7,24 +7,33 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
-import 'package:flutter/widgets.dart';
-
 class $SoundsGen {
   const $SoundsGen();
 
+  /// Directory path: sounds/ambiances
   $SoundsAmbiancesGen get ambiances => const $SoundsAmbiancesGen();
+
+  /// Directory path: sounds/combat
   $SoundsCombatGen get combat => const $SoundsCombatGen();
+
+  /// Directory path: sounds/footsteps
   $SoundsFootstepsGen get footsteps => const $SoundsFootstepsGen();
+
+  /// Directory path: sounds/menus
   $SoundsMenusGen get menus => const $SoundsMenusGen();
+
+  /// Directory path: sounds/music
   $SoundsMusicGen get music => const $SoundsMusicGen();
+
+  /// Directory path: sounds/zombies
   $SoundsZombiesGen get zombies => const $SoundsZombiesGen();
 }
 
 class $SoundsAmbiancesGen {
   const $SoundsAmbiancesGen();
 
-  /// File path: sounds/ambiances/main_level
-  String get mainLevel => 'sounds/ambiances/main_level';
+  /// File path: sounds/ambiances/main_level.wav
+  String get mainLevel => 'sounds/ambiances/main_level.wav';
 
   /// List of all assets
   List<String> get values => [mainLevel];
@@ -107,10 +116,17 @@ class $SoundsMusicGen {
 class $SoundsZombiesGen {
   const $SoundsZombiesGen();
 
+  /// Directory path: sounds/zombies/breathing
   $SoundsZombiesBreathingGen get breathing =>
       const $SoundsZombiesBreathingGen();
+
+  /// Directory path: sounds/zombies/death
   $SoundsZombiesDeathGen get death => const $SoundsZombiesDeathGen();
+
+  /// Directory path: sounds/zombies/hits
   $SoundsZombiesHitsGen get hits => const $SoundsZombiesHitsGen();
+
+  /// Directory path: sounds/zombies/sayings
   $SoundsZombiesSayingsGen get sayings => const $SoundsZombiesSayingsGen();
 }
 
@@ -200,78 +216,4 @@ class Assets {
   Assets._();
 
   static const $SoundsGen sounds = $SoundsGen();
-}
-
-class AssetGenImage {
-  const AssetGenImage(this._assetName);
-
-  final String _assetName;
-
-  Image image({
-    Key? key,
-    AssetBundle? bundle,
-    ImageFrameBuilder? frameBuilder,
-    ImageErrorWidgetBuilder? errorBuilder,
-    String? semanticLabel,
-    bool excludeFromSemantics = false,
-    double? scale,
-    double? width,
-    double? height,
-    Color? color,
-    Animation<double>? opacity,
-    BlendMode? colorBlendMode,
-    BoxFit? fit,
-    AlignmentGeometry alignment = Alignment.center,
-    ImageRepeat repeat = ImageRepeat.noRepeat,
-    Rect? centerSlice,
-    bool matchTextDirection = false,
-    bool gaplessPlayback = false,
-    bool isAntiAlias = false,
-    String? package,
-    FilterQuality filterQuality = FilterQuality.low,
-    int? cacheWidth,
-    int? cacheHeight,
-  }) {
-    return Image.asset(
-      _assetName,
-      key: key,
-      bundle: bundle,
-      frameBuilder: frameBuilder,
-      errorBuilder: errorBuilder,
-      semanticLabel: semanticLabel,
-      excludeFromSemantics: excludeFromSemantics,
-      scale: scale,
-      width: width,
-      height: height,
-      color: color,
-      opacity: opacity,
-      colorBlendMode: colorBlendMode,
-      fit: fit,
-      alignment: alignment,
-      repeat: repeat,
-      centerSlice: centerSlice,
-      matchTextDirection: matchTextDirection,
-      gaplessPlayback: gaplessPlayback,
-      isAntiAlias: isAntiAlias,
-      package: package,
-      filterQuality: filterQuality,
-      cacheWidth: cacheWidth,
-      cacheHeight: cacheHeight,
-    );
-  }
-
-  ImageProvider provider({
-    AssetBundle? bundle,
-    String? package,
-  }) {
-    return AssetImage(
-      _assetName,
-      bundle: bundle,
-      package: package,
-    );
-  }
-
-  String get path => _assetName;
-
-  String get keyName => _assetName;
 }
